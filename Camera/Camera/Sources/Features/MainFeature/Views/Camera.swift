29380f65da5eceb94b3e007f8c8d71ae7d77a4e5
Kody Deda <kodydeda4@gmail.com>
2024-12-16 13:59:24 -0500

    wip

--- Camera/Camera/Sources/Features/MainFeature/Views/Camera.swift
+++ Camera/Camera/Sources/Features/MainFeature/Views/Camera.swift
@@ -1,9 +1,27 @@
 import SwiftUI
 import Sharing
+import AVFoundation
 
 extension MainView {
   @MainActor internal var camera: some View {
-    Text("hi")
+    AVCaptureVideoPreviewLayerView(avVideoPreviewLayer: self.model.avVideoPreviewLayer)
+  }
+}
+
+private struct AVCaptureVideoPreviewLayerView: UIViewControllerRepresentable {
+  let avVideoPreviewLayer: AVCaptureVideoPreviewLayer
+  typealias UIViewControllerType = UIViewController
+
+  func makeUIViewController(context: Context) -> UIViewController {
+    let viewController = UIViewController()
+    viewController.view.backgroundColor = .black
+    viewController.view.layer.addSublayer(avVideoPreviewLayer)
+    avVideoPreviewLayer.frame = viewController.view.bounds
+    return viewController
+  }
+
+  func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    //...
   }
 }
 
