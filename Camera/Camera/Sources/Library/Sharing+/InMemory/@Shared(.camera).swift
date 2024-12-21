3c310b837c316fcef0986264367a553af95ac425
Kody Deda <kodydeda4@gmail.com>
2024-12-21 10:01:52 -0500

    add hints about bug

--- Camera/Camera/Sources/Library/Sharing+/InMemory/@Shared(.camera).swift
+++ Camera/Camera/Sources/Library/Sharing+/InMemory/@Shared(.camera).swift
@@ -3,6 +3,7 @@ import Sharing
 
 //@DEDA
 // you can probably fix a bunch of bugs if you hold onto AVCaptureDevice or smthn.
+// when you switch cameras, you forget the previous setup, so when you switch back and forth the ui has not been updated with the correct zoom etc.
 struct CameraState: Equatable {
   var zoom = 1.0
   var isRecording = false
