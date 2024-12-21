7d29ed3cd91d02dff4f288041259b03be02c8c28
Kody Deda <kodydeda4@gmail.com>
2024-12-21 10:00:25 -0500

    fix again again again

--- Camera/Camera/Sources/Library/Sharing+/InMemory/@Shared(.camera).swift
+++ Camera/Camera/Sources/Library/Sharing+/InMemory/@Shared(.camera).swift
@@ -6,7 +6,7 @@ import Sharing
 struct CameraState: Equatable {
   var zoom = 1.0
   var isRecording = false
-  var position = AVCaptureDevice.Position.front
+  var position = AVCaptureDevice.Position.back
   var captureVideoPreviewLayer = AVCaptureVideoPreviewLayer()
 }
 
