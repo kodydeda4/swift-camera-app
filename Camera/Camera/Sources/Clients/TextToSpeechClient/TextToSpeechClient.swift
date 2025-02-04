c67f391176f7505ef912a9848124c896d4d56f9d
Kody Deda <kodydeda4@gmail.com>
2025-02-04 12:48:41 -0500

    add comments for tts client

---
+++ Camera/Camera/Sources/Clients/TextToSpeechClient/TextToSpeechClient.swift
@@ -0,0 +1,55 @@
+// Copyright © 2020 Pocket Radar. All rights reserved.
+
+import Dependencies
+import DependenciesMacros
+import AVFoundation
+
+@DependencyClient
+public struct TextToSpeechClient {
+  var speak: (AVSpeechUtterance) -> Void = { _ in }
+  var speakAsync: (AVSpeechUtterance) async -> Void = { _ in }
+}
+
+public extension DependencyValues {
+  var textToSpeech: TextToSpeechClient {
+    get { self[TextToSpeechClient.self] }
+    set { self[TextToSpeechClient.self] = newValue }
+  }
+}
+
+// MARK: - Implementation
+
+extension TextToSpeechClient: DependencyKey {
+  //@DEDA you need to add loggers.
+  public static var liveValue: Self {
+//    let log = Loggers.textToSpeechClient
+    let synthesizer = AVSpeechSynthesizer()
+    let delegate = Delegate()
+    
+    return Self(
+      speak: { utterance in
+//        log.info("speak(\(utterance))")
+        print("speak(\(utterance))")
+        synthesizer.speak(utterance)
+      },
+      speakAsync: { utterance in
+//        log.info("speakAsync(\(utterance)")
+        print("speakAsync(\(utterance)")
+        await withCheckedContinuation { continuation in
+          delegate.continuation = continuation
+          synthesizer.delegate = delegate
+          synthesizer.speak(utterance)
+        }
+      }
+    )
+    
+    final class Delegate: NSObject, AVSpeechSynthesizerDelegate {
+      var continuation: CheckedContinuation<Void, Never>?
+
+      func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        continuation?.resume()
+        continuation = .none
+      }
+    }
+  }
+}
